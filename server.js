c5c398d9f91b5559c42cc6755b4e80ffe0b2c4a7
Chiahao <chiahao.codes@gmail.com>
2024-05-08 12:24:56 -0400

    commit main

--- server.js
+++ server.js
@@ -214,7 +214,7 @@ app.get("/tickrpro/:symbol", async (req, res) => {
   cashFlowResult = results[8];
   defaultStats = results[9];
 
-  console.log(quoteCheck.quoteResponse.result[0]);
+  console.log(incomeResult);
 
   /**
    * if (
