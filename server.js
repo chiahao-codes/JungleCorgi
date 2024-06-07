6ccf7bf7b69efa88e2bb77642489c85df6eaf7e2
Chiahao <chiahao.codes@gmail.com>
2024-06-06 22:02:47 -0700

    commit main

--- server.js
+++ server.js
@@ -101,29 +101,125 @@ app.get("/tickrpro/contact", async (req, res, next) => {
   res.render("contact");
 });
 
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "626350d676msh4d1dc66afe62e86p1adf8ejsndc3d7f1bb723",
+    "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+  },
+};
+
+const grabChartData = async (symbol, range) => {
+  let url;
+  switch (range) {
+    case "5d":
+      url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=60m&region=US&symbol=${symbol}&range=5d&includePrePost=false&useYfid=true&includeAdjustedClose=true`;
+      break;
+    case "1mo":
+      url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=1d&region=US&symbol=${symbol}&range=1mo&includePrePost=false&useYfid=true&includeAdjustedClose=true`;
+      break;
+    case "YTD":
+      url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=1wk&region=US&symbol=${symbol}&range=ytd&includePrePost=false&useYfid=true&includeAdjustedClose=true`;
+      break;
+    case "2y":
+      url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=1mo&region=US&symbol=${symbol}&range=2y&includePrePost=false&useYfid=true&includeAdjustedClose=true`;
+      break;
+    default:
+      console.log("Range not found");
+  }
+  /**
+   *  const fiveDayUrl = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=60m&region=US&symbol=${symbol}&range=5d&includePrePost=false&useYfid=true&includeAdjustedClose=true`;
+  const oneMoUrl = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=1d&region=US&symbol=${symbol}&range=1mo&includePrePost=false&useYfid=true&includeAdjustedClose=true`;
+  const ytdUrl = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=1wk&region=US&symbol=${symbol}&range=ytd&includePrePost=false&useYfid=true&includeAdjustedClose=true`;
+  const twoYrUrl = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=1mo&region=US&symbol=${symbol}&range=2y&includePrePost=false&useYfid=true&includeAdjustedClose=true`;
+   */
+
+  let response = await fetch(url, options)
+    .then((resp) => resp.json())
+    .catch((e) => console.log(e));
+
+  console.log(response);
+  return response;
+};
+
+const fillChartData = async (responseData) => {
+  let chartResp, chartTimes, closeQuote, openQuote;
+
+  //5m:1day, 1h:5days;
+  //1d:months, 1mo:years;
+  let chartData = {
+    datasets: [
+      {
+        label: "",
+        data: [],
+        borderColor: dataLineColor,
+        backgroundColor: dataLineColor,
+        color: dataLineColor,
+        tension: 0.1,
+        pointBorderWidth: 0.5,
+      },
+    ],
+    labels: [],
+  };
+
+  chartResp = responseData.chart.result[0];
+  chartTimes = responseData.chart.result[0].timestamp;
+  openQuote = responseData.chart.result[0].indicators.quote[0].open;
+  closeQuote = responseData.chart.result[0].indicators.quote[0].close;
+  //Iterate over timestamp array, start from the last one, go backwards;
+  let timestampEnd = chartTimes.length - 1;
+
+  //return chartdata object;
+  //start from the last one and go backwards;
+  for (let i = timestampEnd; i >= 0; i--) {
+    let currTimestamp = chartTimes[i];
+    //setMarketTime(currTimestamp);
+    let date, formatted;
+    date = new Date(currTimestamp * 1000);
+    formatted = new Intl.DateTimeFormat("en-US", {
+      dateStyle: "short",
+      timeStyle: "long",
+      timeZone: "UTC",
+    }).format(date);
+
+    let labelDate = formatted;
+
+    let mktPrice = closeQuote[i]; //closing price;
+
+    //get UTC Date to determine mkt price Open vs. Closed
+    currTimestamp *= 1000;
+    let dateCreate = new Date(currTimestamp);
+
+    //find curr utc timestamp vs. market open of stock symbol
+    let utcHours = dateCreate.getUTCHours();
+    let utcMin = dateCreate.getUTCMinutes();
+
+    let marketOpen = chartResp.meta.currentTradingPeriod.regular.start;
+    let mktOpenDate = new Date(marketOpen * 1000);
+    let utcHourOpen = mktOpenDate.getUTCHours();
+    let utcMinsOpen = mktOpenDate.getUTCMinutes();
+
+    if (utcHourOpen === utcHours && utcMinsOpen === utcMin) {
+      mktPrice = openQuote[i];
+    }
+
+    chartData.datasets[0].data.unshift(mktPrice);
+    chartData.labels.unshift(labelDate);
+  }
+
+  return chartData;
+};
+
 app.get("/tickrpro/:symbol", async (req, res) => {
   let symbol = req.params.symbol,
     getSumm,
-    chartDataFiveMin,
-    results,
-    chartResult5m,
+    chartData1d,
+    chartRes1d,
     incomeResult,
     balanceResult,
     cashFlowRes;
 
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "626350d676msh4d1dc66afe62e86p1adf8ejsndc3d7f1bb723",
-      "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-    },
-  };
-
-  //charts data;
-  //Same day chart;
-  //int:5m, range:1d
-  //int:60m, range:5d
-  const fiveMinUrl = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=5m&symbol=${symbol}&range=1d&region=US&includePrePost=false&useYfid=true&includeAdjustedClose=true`;
+  const oneDayUrl = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart?interval=5m&symbol=${symbol}&range=1d&region=US&includePrePost=false&useYfid=true&includeAdjustedClose=true`;
   const incomeStmtURL = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-financials?symbol=${symbol}&region=US'`;
   const cashFlowURL = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-cash-flow?symbol=${symbol}&region=US`;
   const balanceShtURL = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-balance-sheet?symbol=${symbol}&region=US`;
@@ -131,53 +227,46 @@ app.get("/tickrpro/:symbol", async (req, res) => {
 
   //check quote type:
   getSumm = await fetch(getSummURL, options)
-    .then((response) => {
-      return response.json();
-    })
+    .then((res) => res.json())
     .catch((e) => console.log(e));
-  //getSumm = await getSumm.json();
-  console.log("getSumm:", getSumm);
 
   if (getSumm.quoteType === "ECNQUOTE" || getSumm.quoteType === "MUTUALFUND") {
     return res.render("404");
   }
-  chartDataFiveMin = await fetch(fiveMinUrl, options).then((resp) => {
-    return resp.json();
-  });
 
-  const getIncomeStmt = fetch(incomeStmtURL, options).then((resp) => {
-    return resp.json();
-  });
+  chartData1d = await fetch(oneDayUrl, options)
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
 
-  const getBalanceSht = fetch(balanceShtURL, options).then((resp) => {
-    return resp.json();
-  });
+  chartRes1d = chartData1d.chart.result[0];
+  console.log("chartRes1d", chartRes1d);
 
-  const getCashFlow = fetch(cashFlowURL, options).then((resp) => {
-    return resp.json();
-  });
+  let getIncomeStmt = await fetch(incomeStmtURL, options)
+    .then((resp) => resp.json())
+    .catch((e) => console.log(e));
 
-  results = await Promise.all([
-    chartDataFiveMin,
-    getIncomeStmt,
-    getBalanceSht,
-    getCashFlow,
-  ]).catch((e) => console.log(e));
+  incomeResult = getIncomeStmt;
 
-  chartResult5m = results[0];
-  incomeResult = results[1];
-  balanceResult = results[2];
-  cashFlowRes = results[3];
+  let getBalanceSht = await fetch(balanceShtURL, options)
+    .then((resp) => resp.json())
+    .catch((e) => console.log(e));
 
-  console.log("chartResult5m", chartResult5m.chart.result[0].meta);
-  return res.render("ticker", {
-    API_KEY,
-    RAPID,
+  balanceResult = getBalanceSht;
+
+  let getCashFlow = await fetch(cashFlowURL, options)
+    .then((resp) => resp.json())
+    .catch((e) => console.log(e));
+
+  cashFlowRes = getCashFlow;
+
+  res.render("ticker", {
     getSumm,
-    cashFlowRes,
-    balanceResult,
+    chartRes1d,
     incomeResult,
-    chartResult5m,
+    balanceResult,
+    cashFlowRes,
+    grabChartData,
+    fillChartData,
   });
 });
 
