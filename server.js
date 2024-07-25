7b5dcd5c7d538e20e888b4d9c8bef8818c489e62
Chiahao <chiahao.codes@gmail.com>
2024-07-24 20:18:01 -0700

    commit main

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.static("assets"));
 app.use(express.json());
-app.use(cors({ origin: "https://junglecorgi.com" }));
+app.use(cors());
 
 const apiOptions = {
   credentials: "include",
