89ef3842b53382d08211948cbf570f77e99c3b35
Chiahao <chiahao.codes@gmail.com>
2024-07-24 20:44:02 -0700

    commit main

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.static("assets"));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 const apiOptions = {
-  credentials: "include",
   method: "GET",
   headers: {
     "x-rapidapi-key": API_KEY,
